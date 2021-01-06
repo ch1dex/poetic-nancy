d1dbca5361b72da7ce12f77ebdf02ad9310c4dd3
ch1dex <chidexjohn942@gmail.com>
2021-01-06 01:57:26 +0100

    'message'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,15 +6,15 @@ function NavBar() {
     return (
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='nav' >
-            <Navbar.Brand href="#" className='nav-color'>
-                <Image src='../bg-img/logo.png' />
+            <Navbar.Brand href="/" className='nav-color'>
+                <Image src='/logo.png' />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav" >
                  <Nav className="mr-auto">
                 </Nav> 
                 <Nav className='nav-right'>
-                <Nav.Link href="#" className='nav-color'>Home</Nav.Link>
+                <Nav.Link href="/" className='nav-color'>Home</Nav.Link>
                 <Nav.Link href="#About" className='nav-color'>About me</Nav.Link>
                 <Nav.Link href="#Products" className='nav-color'>Products</Nav.Link>
                 <Nav.Link href="#Service" className='nav-color'>Service</Nav.Link>
